22db3fcd6aa5cef4d6ca2e9651d1c61339578a28
Chris Papenfuß <chris.papenfuss@data-experts.de>
2022-03-24 16:18:07 -0400

    ui getting in shape

---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    public LineRenderer LinePrefab;
+
+    public float StartingAngle = 30f;
+    public float EndingAngle = 150f;
+    public float AngleSteps = 10f;
+    public float HeightSteps = -0.1f;
+
+    public float[] Radius;
+
+    private List<LineRenderer> _linesCreated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyOldLines();
+        Array.Sort(Radius);
+        var lineCnt = 0;
+        _linesCreated = new List<LineRenderer>();
+        foreach (var val in Radius)
+        {
+            var line = GameObject.Instantiate(LinePrefab, transform);
+            _linesCreated.Add(line);
+            var pos = new List<Vector3>();
+            var lineHeight = HeightSteps * lineCnt++;
+            for (var angle = StartingAngle; angle <= EndingAngle; angle += AngleSteps)
+            {
+                pos.Add(new Vector3(Mathf.Sin(angle / 180F * Mathf.PI) * val, lineHeight, Mathf.Cos(angle / 180F * Mathf.PI) * val));
+            }
+            line.positionCount = pos.Count;
+            line.SetPositions(pos.ToArray());
+        }
+    }
+
+    private void DestroyOldLines()
+    {
+        if (_linesCreated != null)
+        {
+            foreach (var line in _linesCreated) { Destroy(line.gameObject); }
+        }
+    }
+
+    private void OnDisable()
+    {
+        DestroyOldLines();
+    }
+
+    private void OnDestroy()
+    {
+        DestroyOldLines();
+    }
+
+}
---
+++ Assets/Scripts/SlowFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowFollow : MonoBehaviour
+{
+    public Transform ObjToFollow;
+
+    public float MinDistAngle = 30F;
+
+    private bool _adjusting = false;
+
+    public float AdjustSpeed = 10F;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = Quaternion.Angle(transform.rotation, ObjToFollow.transform.rotation);
+        if (angle > MinDistAngle && !_adjusting)
+        {
+            StartCoroutine(AdjustRotation(ObjToFollow.transform.rotation));
+        }
+    }
+
+    private IEnumerator AdjustRotation(Quaternion rotation)
+    {
+        _adjusting = true;
+        while (Quaternion.Angle(rotation, transform.rotation) > 5F)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, AdjustSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        _adjusting = false;
+    }
+}
