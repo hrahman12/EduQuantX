130e4ca954193c5a7efe56f2e9eba12dc87988d4
Chris Papenfuß <chris.papenfuss@data-experts.de>
2022-03-24 18:15:55 -0400

    can move gates to circuit

---
+++ Assets/Scripts/CheckOperator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOperator : MonoBehaviour
+{
+    public static CheckOperator Instance;
+
+    public LineRenderer[] ValidLines;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public bool CheckPosition(OperatorControl control, out Vector3 pos)
+    {
+        foreach (var line in ValidLines)
+        {
+            for (var i = 1; i < line.positionCount - 1; i++)
+            {
+                pos = line.transform.TransformPoint(line.GetPosition(i));
+                //Debug.DrawLine(pos, control.transform.position, Color.blue);
+                if (control.IsInPos(pos))
+                {
+                    return true;
+                }
+            }
+        }
+        pos = Vector3.zero;
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/OperatorControl.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class OperatorControl : MonoBehaviour
+{
+    private bool _createdClone = false;
+
+    public float MoveSpeed = 5F;
+
+    private bool _moving = false;
+    private Vector3 _startPos;
+    private Renderer _renderer;
+
+    public bool IsInPos(Vector3 pos)
+    {
+        return _renderer.bounds.Contains(pos);
+    }
+
+    private void OnEnable()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void CheckIfMovedAway()
+    {
+        _moving = true;
+        _startPos = transform.position;
+        if (_createdClone) return;
+        StartCoroutine(CheckIfMovedAsync());
+    }
+
+    public void CheckPositionAfterMove()
+    {
+        _moving = false;
+        if (CheckOperator.Instance.CheckPosition(this, out var pos))
+        {
+            Destroy(GetComponent<ObjectManipulator>());
+            StartCoroutine(MoveTo(pos));
+        }
+        else
+        {
+            StartCoroutine(MoveTo(_startPos, true));
+        }
+    }
+
+    private IEnumerator MoveTo(Vector3 pos, bool destroyAfterReach = false)
+    {
+        while (Vector3.Distance(pos, transform.position) > 0.01F)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos, MoveSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        if (destroyAfterReach)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (_moving && CheckOperator.Instance.CheckPosition(this, out var pos))
+        {
+
+        }
+    }
+
+    private IEnumerator CheckIfMovedAsync()
+    {
+        var startPos = transform.position;
+        var startRot = transform.rotation;
+        var bnds = new Bounds(_renderer.bounds.center, _renderer.bounds.size);
+        while (bnds.Intersects(_renderer.bounds))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        GameObject.Instantiate(gameObject, startPos, startRot, transform.parent);
+        _createdClone = true;
+        _moving = true;
+    }
+
+}
