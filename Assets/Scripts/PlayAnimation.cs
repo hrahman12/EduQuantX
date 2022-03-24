bbca1f53a40cc6b25a1e859d88eaef0047c96f09
Chris Papenfuß <chris.papenfuss@data-experts.de>
2022-03-24 10:15:40 -0400

    adding simple sample to check build process

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    public string AnimationToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.Play(AnimationToPlay);
+    }
+
+}
