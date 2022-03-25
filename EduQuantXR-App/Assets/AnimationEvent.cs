bc3dacc93137b3aa537857bd0a9815566b8e6b2b
Chris Papenfuß <chris.papenfuss@data-experts.de>
2022-03-25 10:26:57 -0400

    coin toss animation done

---
+++ EduQuantXR-App/Assets/AnimationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEvent : MonoBehaviour
+{
+
+    public UnityEvent[] AnimationEvents;
+
+    public void ThrowAnimationEvent(int i)
+    {
+        if (AnimationEvents.Length <= i || i < 0)
+        {
+            Debug.LogError("AnimationEvent not specified");
+            return;
+        }
+        AnimationEvents[i]?.Invoke();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EduQuantX
 Quantum Computing game with Schrodinger's cat in VR
+
+# Assets in Use
+These Assets where not created but are free to use
+## sounds
+- coin Toss [https://freesound.org/people/InspectorJ/sounds/342242/]
